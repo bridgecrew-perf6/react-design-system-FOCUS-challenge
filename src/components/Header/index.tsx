edb2be08d18ce37c49f5a7931ba641718792cfdd
Min <alstnsrl98@gmail.com>
2022-01-07 23:16:35 +0900

    Feat: create input container

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,14 @@
-import { StyledHeaderWrapper } from "./styles";
+import { SearchIcon, IconButton, Input } from "@class101/ui";
+import { StyledFormGroup, StyledHeaderWrapper } from "./styles";
 
 const Header = () => {
-  return <StyledHeaderWrapper />;
+  return (
+    <StyledHeaderWrapper>
+      <StyledFormGroup label="" inline>
+        <Input placeholder="찾으시는 취미가 있으신가요?" />
+        <IconButton icon={<SearchIcon />} />
+      </StyledFormGroup>
+    </StyledHeaderWrapper>
+  );
 };
 export default Header;
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,7 +1,35 @@
+import { FormGroup } from "@class101/ui";
 import styled from "styled-components";
 
 export const StyledHeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
   width: 100%;
-  height: 100px;
   border: 1px solid black;
+`;
+
+export const StyledFormGroup = styled(FormGroup)`
+  margin: 0;
+  position: relative;
+  width: 420px;
+  div {
+    width: 100%;
+  }
+  input {
+    width: 100%;
+    font-size: 14px;
+    line-height: 20px;
+    background: rgb(248, 248, 248);
+    border: 1px solid rgb(248, 248, 248);
+    border-radius: 24px;
+    padding: 12px 52px 12px 16px;
+    &::placeholder {
+      color: rgb(117, 117, 117);
+    }
+  }
+  button {
+    position: absolute;
+    right: 16px;
+  }
 `;
